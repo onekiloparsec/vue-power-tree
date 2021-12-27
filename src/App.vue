27c4b4af10fd1eada4be2e2906d8c9f58b90a977
Cédric Foellmi <cedric@onekiloparsec.dev>
2021-12-27 15:24:39 +0100

    Updatd demo
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,12 @@
     <div class="row">
       <div class="tree-container">
         <power-tree v-model="nodes" :show-branches="true">
+          <template #title="{ node }">
+            <div data-title="root">
+              {{ node.title }}
+            </div>
+          </template>
+
           <template #sidebar>
             <div data-sidebar="true">
               <button @click="click" data-sidebar="button">-</button>
@@ -76,37 +82,6 @@
 </script>
 
 <style>
-  /*html {*/
-  /*  font-family: sans-serif;*/
-  /*  font-size: 10px;*/
-  /*  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);*/
-  /*  -ms-text-size-adjust: 100%;*/
-  /*  -webkit-text-size-adjust: 100%;*/
-  /*  height: 100%;*/
-  /*}*/
-
-  /*body {*/
-  /*  position: relative;*/
-  /*  height: 100%;*/
-  /*  color: #555555;*/
-  /*  margin: 0;*/
-  /*  font-family: 'PT Sans', sans-serif;*/
-  /*  font-size: 14px;*/
-  /*  line-height: 1.42857143;*/
-  /*  background-color: #ffffff;*/
-  /*}*/
-
-  /*#app {*/
-  /*  !*font-family: 'Avenir', Helvetica, Arial, sans-serif;*!*/
-  /*  !*-webkit-font-smoothing: antialiased;*!*/
-  /*  !*-moz-osx-font-smoothing: grayscale;*!*/
-  /*  !*text-align: center;*!*/
-  /*  color: #2c3e50;*/
-  /*  min-width: 500px;*/
-  /*  width: 50%;*/
-  /*  margin: 60px auto;*/
-  /*}*/
-
   body {
     background: #050d12;
     font-family: Arial;
